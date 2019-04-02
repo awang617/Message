2b0d539ac0898a5ae8686302d2a0201d5203351f
awang617 <wanga18@mail.wlu.edu>
2019-04-01 17:16:37 -0700

    modified route after login slightly

--- app.py
+++ app.py
@@ -61,10 +61,10 @@ def login():
                 flash('You have been logged in!', 'success')
                 order = models.Order.select().where(models.Order.user == user.id, models.Order.purchased==False)
                 if order.exists():
-                    return redirect(url_for('profile'))
+                    return redirect(url_for('shop'))
                 else:
                     newOrder = models.Order.create_order(user=user)
-                    return redirect(url_for('profile'))
+                    return redirect(url_for('shop'))
             else:
                 flash('Email or password incorrect.', 'danger')
     return render_template("login.html", form=form)
@@ -85,7 +85,7 @@ def signup():
         models.Order.create_order(user=user)
         login_user(user)
         # redirect user to index
-        return redirect(url_for('profile'))
+        return redirect(url_for('shop'))
     return render_template("signup.html", form=form)
 
 @app.route('/logout')
@@ -105,11 +105,15 @@ def profile():
 @app.route('/shop', methods=["GET"])
 @login_required
 def shop():
-    
     product = models.Product.select()
-    
-
     return render_template("shop.html", product=product)
+
+@app.route('/shop/<data_name>', methods=["GET"])
+@login_required
+def product_details(data_name):
+    product = models.Product.get(models.Product.data_name == data_name)
+    print(product)
+    return render_template("productDetails.html", product=product)
 
 @app.route('/cart', methods=["GET"])
 @login_required
@@ -119,64 +123,70 @@ def cart():
 if __name__ == '__main__':
     models.initialize()
     # try:
-        # models.Category.create(category_name="bouquet")
-        # models.Category.create(category_name="plant")
-        # bouquet = models.Category.get(models.Category.category_name == "bouquet")
-        # plant = models.Category.get(models.Category.category_name == "plant")
-        # models.Product.create(
-        #     name="Roses are Red",
-        #     description="Everyone knows red roses means love. This classic bouquet is perfect for romantic occasions.",
-        #     image="https://cdn.azflorist.com/wp-content/uploads/20190128091943/E2-4305D.jpg",
-        #     plant="red rose",
-        #     meaning="love",
-        #     price=40,
-        #     category=bouquet
-        # )
-        # models.Product.create(
-        #     name="Prettily Presuming",
-        #     description="Be careful when giving or recieving this beautiful bloom, as snapdragons mean presumption. Perhaps there is someone sets themselves too highly, or took you for granted. A snapdragon may snap them into place.",
-        #     image="https://cdn1.harryanddavid.com/wcsstore/HarryAndDavid/images/catalog/17_31841_30E_01ex.jpg",
-        #     plant="snapdragon",
-        #     meaning="Presumption",
-        #     price=35,
-        #     category=bouquet
-        # )
-        # models.Product.create(
-        #     name="Return of Happiness",
-        #     description="These delicate flowers are perfect if you wish for a return of joy. Send it after an argument has pass or as a homecoming gift.",
-        #     image="https://i.pinimg.com/originals/64/7e/b5/647eb574340a0351ba55e3d1d91fae77.jpg",
-        #     plant="lily of the valley",
-        #     meaning="Return of happiness",
-        #     price=49,
-        #     category=bouquet
-        # )
-        # models.Product.create(
-        #     name="Ardent Love",
-        #     description="Don't let its prickly exterior fool you. Cactus means ardent love, and is a great alternative if your partner thinks roses are cliche. Best of all, they are easy to care for and live long lives.",
-        #     image="https://cdn.shopify.com/s/files/1/0130/1052/products/DSC_0340_1024x1024@2x.jpg?v=1517494867",
-        #     plant="cactus",
-        #     meaning="ardent love",
-        #     price="19",
-        #     category=plant
-        # )
-        # models.Product.create(
-        #     name="Prosper",
-        #     description="A gift imbued with the best wishes of prosperity and good health.",
-        #     image="https://cdn.shopify.com/s/files/1/0207/8508/products/2018-Sage-Bush-001_1_1024x1024.jpg?v=1537882781",
-        #     plant="sage",
-        #     meaning="good health and long life",
-        #     price=15,
-        #     category=plant
-        # )
-        # models.Product.create(
-        #     name="Lavender",
-        #     description="Although its scent is soothing its meaning is not. Lavender means mistrust, so be wary who you send one to.",
-        #     image="https://bouqs-production-weblinc.netdna-ssl.com/product_images/lavender/Original/5c3d2db061707040b4002757/detail.jpg?c=1547513264",
-        #     plant="lavender",
-        #     meaning="mistrust",
-        #     price=22,
-        #     category=plant
-        # )
+    #     models.Category.create(category_name="bouquet")
+    #     models.Category.create(category_name="plant")
+    #     bouquet = models.Category.get(models.Category.category_name == "bouquet")
+    #     plant = models.Category.get(models.Category.category_name == "plant")
+    #     models.Product.create(
+    #         name="Roses are Red",
+    #         description="Everyone knows red roses means love. This classic bouquet is perfect for romantic occasions.",
+    #         image="https://cdn.azflorist.com/wp-content/uploads/20190128091943/E2-4305D.jpg",
+    #         plant="red rose",
+    #         meaning="love",
+    #         price=40,
+    #         data_name="red-roses",
+    #         category=bouquet
+    #     )
+    #     models.Product.create(
+    #         name="Prettily Presuming",
+    #         description="Be careful when giving or recieving this beautiful bloom, as snapdragons mean presumption. Perhaps there is someone sets themselves too highly, or took you for granted. A snapdragon may snap them into place.",
+    #         image="https://cdn1.harryanddavid.com/wcsstore/HarryAndDavid/images/catalog/17_31841_30E_01ex.jpg",
+    #         plant="snapdragon",
+    #         meaning="Presumption",
+    #         price=35,
+    #         data_name="prettily-presuming",
+    #         category=bouquet
+    #     )
+    #     models.Product.create(
+    #         name="Return of Happiness",
+    #         description="These delicate flowers are perfect if you wish for a return of joy. Send it after an argument has pass or as a homecoming gift.",
+    #         image="https://i.pinimg.com/originals/64/7e/b5/647eb574340a0351ba55e3d1d91fae77.jpg",
+    #         plant="lily of the valley",
+    #         meaning="Return of happiness",
+    #         price=49,
+    #         data_name="return-of-happiness",
+    #         category=bouquet
+    #     )
+    #     models.Product.create(
+    #         name="Ardent Love",
+    #         description="Don't let its prickly exterior fool you. Cactus means ardent love, and is a great alternative if your partner thinks roses are cliche. Best of all, they are easy to care for and live long lives.",
+    #         image="https://cdn.shopify.com/s/files/1/0130/1052/products/DSC_0340_1024x1024@2x.jpg?v=1517494867",
+    #         plant="cactus",
+    #         meaning="ardent love",
+    #         price="19",
+    #         data_name="ardent-love",
+    #         category=plant
+    #     )
+    #     models.Product.create(
+    #         name="Prosper",
+    #         description="A gift imbued with the best wishes of prosperity and good health.",
+    #         image="https://cdn.shopify.com/s/files/1/0207/8508/products/2018-Sage-Bush-001_1_1024x1024.jpg?v=1537882781",
+    #         plant="sage",
+    #         meaning="good health and long life",
+    #         price=15,
+    #         data_name="prosper",
+    #         category=plant
+    #     )
+    #     models.Product.create(
+    #         name="Lavender",
+    #         description="Although its scent is soothing its meaning is not. Lavender means mistrust, so be wary who you send one to.",
+    #         image="https://bouqs-production-weblinc.netdna-ssl.com/product_images/lavender/Original/5c3d2db061707040b4002757/detail.jpg?c=1547513264",
+    #         plant="lavender",
+    #         meaning="mistrust",
+    #         price=22,
+    #         data_name="potted-lavender",
+    #         category=plant
+    #     )
     # except ValueError:
     #     pass
     app.run(debug=DEBUG, port=PORT)
