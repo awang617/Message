a327b40f42bbbf5e19c9eece2a0d91a862df1c22
awang617 <wanga18@mail.wlu.edu>
2019-04-04 15:52:55 -0700

    edit functionality for the user

--- forms.py
+++ forms.py
@@ -52,11 +52,57 @@ class LoginForm(Form):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
 
+class EditProfileForm(Form):
+    fname = StringField(
+        'First Name',
+        validators=[
+            DataRequired(),
+            Regexp(
+                r'^[a-zA-Z_]+$',
+                message=("Cannot contain numbers or special characters")
+            )
+        ]
+    )
+    lname = StringField(
+        'Last Name',
+        validators=[
+            DataRequired(),
+            Regexp(
+                r'^[a-zA-Z_]+$',
+                message=("Cannot contain numbers or special characters")
+            )
+        ]
+    )
+    email = StringField(
+        'Email',
+        validators=[
+            DataRequired(),
+            Email(),
+            email_exists
+        ])
+
+class EditAddressForm(Form):
+    street_address = StringField("Street Address", validators=[DataRequired()])
+    city = StringField("City", validators=[DataRequired()])
+    state = StringField("State",validators=[DataRequired()])
+    postal_code = StringField(
+        "ZIP Code", 
+        validators=[
+            DataRequired(),
+            Regexp(
+                r'^(\d{5})$',
+                message=("Must be a five digit ZIP Code.")
+    )])
+
 class ReviewForm(Form):
     title = StringField('Title', validators=[DataRequired()])
-    rating = SelectField('Rating', choices=[('1', '1'), ('2','2'), ('3', '3'), ('4', '4'), ('5', '5')])
+    rating = SelectField('Rating', choices=[('5', '5'), ('4','4'), ('3', '3'), ('2', '2'), ('1', '1')])
     content = TextAreaField('Review', validators=[DataRequired()])
     submit = SubmitField("Submit")
 
+class EditReviewForm(Form):
+    title = StringField('Title', validators=[DataRequired()])
+    rating = SelectField('Rating', choices=[('5', '5'), ('4','4'), ('3', '3'), ('2', '2'), ('1', '1')])
+    content = TextAreaField('Review', validators=[DataRequired()])
 
 
