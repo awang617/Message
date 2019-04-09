6ffe028b4956c7c133d6fdd0c02ca5f63e463cd5
awang617 <wanga18@mail.wlu.edu>
2019-04-08 22:46:05 -0700

    got quiz logic working

--- static/quiz.js
+++ static/quiz.js
@@ -4,14 +4,12 @@ $(document).ready(
 
 
 $(`input[type='radio']`).on('click', function() {
-    console.log("keyup")
     $(this).siblings('button').removeClass('hide')
 }) 
 
 
 $('.next').click( function(event) {
     event.preventDefault()
-    console.log("clicked")
     $(this).parent().addClass('hide')
     $(this).parent().next().removeClass('hide')
 
@@ -25,25 +23,22 @@ $('#quiz').submit(function(e) {
 
     var resultNumber = 0
     for(var answer of answers){
-        console.log(answer.value)
         resultNumber += parseInt(answer.value)
     }
     console.log(resultNumber)
     result = getResults(resultNumber)
-    console.log(result)
     displayResults(result)
 })
 
 function getResults(num) {
     console.log('here')
-    if( 0 < num && num <= 4){
-        return "bay leaf"
-    }else if (5 < num && num <= 8) {
-        return "azalea"
-    } else if (8 < num && num <= 12) {
-        console.log(num)
+    if( 8 < num && num <= 13){
+        return "cosmos"
+    }else if (14 < num && num <= 19){
+        return "dogwood"
+    } else if (20 < num && num <= 25){
         return "bluebell"
-    } else if (13 < num && num <= 16){
+    } else if (26 < num && num <= 32){
         return "mistletoe"
     }
 }
