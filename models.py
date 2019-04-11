58805513fc5fd7d12df448f33d7c327315d1ceff
awang617 <wanga18@mail.wlu.edu>
2019-04-11 11:55:38 -0700

    switched database to remote

--- models.py
+++ models.py
@@ -8,8 +8,8 @@ import psycopg2
 
 from playhouse.db_url import connect
 
-# DATABASE = connect(os.environ.get('DATABASE_URL'))
-DATABASE = PostgresqlDatabase('message')
+DATABASE = connect(os.environ.get('DATABASE_URL'))
+# DATABASE = PostgresqlDatabase('message')
 
 class User(UserMixin, Model):
     fname = CharField()
