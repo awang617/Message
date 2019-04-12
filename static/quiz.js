4ec9bfee219d0dc42db4356715cd5f66309e8783
awang617 <wanga18@mail.wlu.edu>
2019-04-11 17:45:58 -0700

    fixed broken logic from centering the buttons

--- static/quiz.js
+++ static/quiz.js
@@ -4,7 +4,7 @@ $(document).ready(
 
 
 $(`input[type='radio']`).on('click', function() {
-    $(this).siblings('button').removeAttr('disabled')
+    $(this).siblings().find('button').removeAttr('disabled')
 }) 
 
 $('.start').click( function(event) {
@@ -17,16 +17,16 @@ $('.start').click( function(event) {
 
 $('.next').click( function(event) {
     event.preventDefault()
-    $(this).parent().parent().addClass('hide')
+    $(this).parent().parent().parent().addClass('hide')
     // debugger;
-    $(this).parent().parent().next().removeClass('hide')
-    $(this).addClass('hide')
+    $(this).parent().parent().parent().next().removeClass('hide')
+    $(this).attr('disabled','disabled')
 
 })
 
 $('.restart').click( function(event) {
     event.preventDefault()
-    $(this).parent().parent().addClass('hide')
+    $(this).parent().parent().parent().addClass('hide')
     $('#first, form').removeClass('hide')
 })
 
@@ -35,27 +35,23 @@ $('#quiz').submit(function(e) {
     e.preventDefault()
     $(this).children().last().addClass('hide')
     var answers = $(this).serializeArray();
-    console.log(answers)
     var resultNumber = 0
     for(var answer of answers){
         resultNumber += parseInt(answer.value)
     }
-    console.log(resultNumber)
     result = getResults(resultNumber)
-    console.log(result)
     displayResults(result)
     this.reset()
 })
 
 function getResults(num) {
-    console.log('here')
-    if( 8 <= num && num <= 13){
+    if( 8 <= num && num <= 14){
         return "cosmos"
-    }else if (14 <= num && num <= 19){
+    }else if (15 <= num && num <= 20){
         return "dogwood"
-    } else if (20 <= num && num <= 25){
+    } else if (21 <= num && num <= 26){
         return "bluebell"
-    } else if (26 <= num && num <= 32){
+    } else if (27 <= num && num <= 32){
         return "mistletoe"
     }
 }
